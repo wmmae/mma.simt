3d69f388287d5442279dce08154da42d2181dedd
mutsuki <mutsuki@momo86.net>
2021-03-28 21:27:41 +0900

    Add a declaration of `fragment`

--- include/wmma_extension/detail/common.hpp
+++ include/wmma_extension/detail/common.hpp
@@ -4,6 +4,9 @@
 namespace mtk {
 namespace wmma {
 namespace mma_simt {
+template <class Use, int m, int n, int k, class T, class Layout = void>
+class fragment_simt;
+
 namespace detail {
 template <typename T, int size>
 struct __align__(4) __frag_base {
