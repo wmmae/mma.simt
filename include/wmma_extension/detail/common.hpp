f1a83c010bb039a9e57d83c743eb48c6cf17e67b
mutsuki <mutsuki@momo86.net>
2021-03-28 21:27:16 +0900

    Add `detail::cast_to`

--- include/wmma_extension/detail/common.hpp
+++ include/wmma_extension/detail/common.hpp
@@ -1,13 +1,23 @@
 #ifndef __WMMAE_SIMT_DETAIL_COMMON__
 #define __WMMAE_SIMT_DETAIL_COMMON__
+#include <cuda_fp16.h>
 namespace mtk {
 namespace wmma {
 namespace mma_simt {
+namespace detail {
 template <typename T, int size>
 struct __align__(4) __frag_base {
 	T x[size];
 	enum {num_elements = size};
 };
+
+template <class DST_T, class SRC_T>
+__device__ inline DST_T cast_to(const SRC_T src) {return static_cast<DST_T>(src);};
+template <>
+__device__ inline float cast_to<float, half>(const half src) {return __half2float(src);}
+template <>
+__device__ inline half cast_to<half, float>(const float src) {return __float2half(src);}
+} // namespace detail
 } // namespace mma_simt
 } // namespace wmma
 } // namespace mtk
