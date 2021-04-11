4c22466d06cabdd27d60abf26b44d9385b110d43
mutsuki <mutsuki@momo86.net>
2021-04-11 18:09:12 +0900

    Add `const` to `fma::operator()`

--- include/wmma_extension/detail/fma.hpp
+++ include/wmma_extension/detail/fma.hpp
@@ -9,7 +9,7 @@ namespace detail {
 
 template <class A_T, class B_T, class C_T>
 struct fma {
-	__device__ C_T operator() (const A_T a, const B_T b, const C_T c) {
+	__device__ C_T operator() (const A_T a, const B_T b, const C_T c) const {
 		const auto fa = cast<float>(a);
 		const auto fb = cast<float>(b);
 		const auto fc = cast<float>(c);
@@ -19,7 +19,7 @@ struct fma {
 
 template <>
 struct fma<double, double, double> {
-	__device__ double operator() (const double a, const double b, const double c) {
+	__device__ double operator() (const double a, const double b, const double c) const {
 		return a * b + c;
 	}
 };
